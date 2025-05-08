537301947bd0c661b97cecb3d85c463018cc2aec
Joaquim Cardeira <asklocation.net@gmail.com>
2025-05-08 13:18:36 +0100

    gcc morocco

--- setup.sh
+++ setup.sh
@@ -4,9 +4,10 @@ set -e
 mkdir -p data
 
 echo "Downloading .pbf files..."
-# wget -q -nc -O data/brazil.pbf https://download.geofabrik.de/south-america/brazil-latest.osm.pbf || true
-wget -nc -O data/chile.pbf https://download.geofabrik.de/south-america/chile-latest.osm.pbf || true
-wget -nc -O data/portugal.pbf https://download.geofabrik.de/europe/portugal-latest.osm.pbf || true
+#wget -nc -O data/brazil.pbf https://download.geofabrik.de/south-america/brazil-latest.osm.pbf || true
+#wget -nc -O data/chile.pbf https://download.geofabrik.de/south-america/chile-latest.osm.pbf || true
+#wget -nc -O data/portugal.pbf https://download.geofabrik.de/europe/portugal-latest.osm.pbf || true
 wget -nc -O data/gcc.pbf https://download.geofabrik.de/asia/gcc-states-latest.osm.pbf || true
+wget -nc -O data/morocco.pbf https://download.geofabrik.de/africa/morocco-latest.osm.pbf || true
 
 docker compose up -d
